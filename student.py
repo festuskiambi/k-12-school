d50bd35f3d6329ed7dedc709baf4e6b4d932fc83
Festus kiambi <kiamfestoh@gmail.com>
2017-02-04 12:50:38 +0300

    student class object

---
+++ student.py
@@ -0,0 +1,37 @@
+class Student(object):
+    def __init__(self):
+        self.fName
+        self.sName
+        self.gradeLevel
+        self.gpa
+        self.teacher
+    #get fName
+    def getfName(self):
+        return self.fName
+    #set sName
+    def setfName(self,fName):
+        self.fName = fName
+    #get sName
+    def getsName(self):
+        return self.sName
+    #set sName
+    def setsName(self):
+        self.sName = sName
+    #get gradeLevel
+    def getgradeLevel(self):
+        return self.gradeLevel
+    #set gradeLevel
+    def setgradeLevel(self):
+        self.gradeLevel = gradeLevel
+    #set gpa
+    def getsName(self):
+        return self.gpa
+    #set sName
+    def setsName(self):
+        self.gpa = gpa
+    #get teacher
+    def getsName(self):
+        return self.teacher
+    #set teacher
+    def setsName(self):
+        self.teacher = teacher
